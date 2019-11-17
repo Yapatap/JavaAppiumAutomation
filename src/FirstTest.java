58bb4791da1fb7da545790d022074cb3bd8f3f05
Arsky Eugene <yapatap@gmail.com>
2019-11-17 18:09:12 +0300

    Ex6 (testAssertionEx6)

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -553,6 +553,33 @@ public class FirstTest {
                 15
         );
     }
+    @Test
+    public void testAssertionEx6()
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find element",
+                5
+        );
+        String search_line = "Java";
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                search_line,
+                "Cannot find element 'Search…'",
+                5
+        );
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[contains(@text,'Object-oriented programming language')]"),
+                "Cannot find 'Object-oriented programming language' topic",
+                15
+        );
+        String search_result_locator = "//*[@resource-id='org.wikipedia:id/view_page_title_text']";
+        assertElementPresent(
+                By.xpath(search_result_locator),
+                "We not found some results by request" + search_line
+        );
+
+    }
 
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
@@ -651,6 +678,15 @@ public class FirstTest {
         {
             String default_message = "An element '" + by.toString() + "' supposed to be not present";
             throw new AssertionError(default_message + " " + error_message);
+        }
+    }
+    private void assertElementPresent(By by, String error_message)
+    {
+        int amount_of_elements = getAmountOfElements(by);
+        if ( amount_of_elements < 0)
+        {
+            String default_message = "An element '" + by.toString() + "' supposed to be present";
+            throw  new AssertionError (default_message + " " + error_message);
         }
     }
     private String waitForElementAndGetAttribute (By by, String attribute, String error_message, long timeoutInSeconds)
